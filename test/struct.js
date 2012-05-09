44447c062246623e330f4c0875033884f8d841c1
Nathan Rajlich <nathan@tootallnate.net>
2012-05-08 18:38:11 -0700

    add some hard-coded test cases
    Not all of them are currently passing...

---
+++ test/struct.js
@@ -0,0 +1,200 @@
+
+var assert = require('assert')
+  , ref = require('ref')
+  , Struct = require('../')
+  , bindings = require('./build/Release/struct_tests')
+
+describe('Struct', function () {
+
+  afterEach(gc)
+
+  it('should be a function', function () {
+    assert.equal('function', typeof Struct)
+  })
+
+  it('should return a struct constuctor function', function () {
+    var S = Struct()
+    assert.equal('function', typeof S)
+  })
+
+  it('should throw when the same field name is speicified more than once', function () {
+    var S = Struct({ a: ref.types.int })
+    assert.throws(function () {
+      S.defineProperty('a', ref.types.int)
+    })
+  })
+
+  it('should work in a simple case', function () {
+    var SimpleStruct = Struct({
+        'first': ref.types.byte
+      , 'last':  ref.types.byte
+    })
+    assert.equal(2, SimpleStruct.size)
+    assert.equal(1, SimpleStruct.alignment)
+
+    var ss = new SimpleStruct({ first: 50, last: 100 })
+    assert.equal(50, ss.first)
+    assert.equal(100, ss.last)
+  })
+
+  it('should work in a more complex case', function () {
+    var MegaStruct = new Struct([
+        ['byte', 'byteVal']
+      , ['int8', 'int8Val']
+      , ['int16', 'int16Val']
+      , ['uint16', 'uint16Val']
+      , ['int32', 'int32Val']
+      , ['uint32', 'uint32Val']
+      , ['float', 'floatVal']
+      , ['double', 'doubleVal']
+      , ['pointer', 'pointerVal']
+    ])
+    var msTestPtr = new Buffer(1)
+    var ms = new MegaStruct({
+        byteVal: 100
+      , int8Val: -100
+      , int16Val: -1000
+      , uint16Val: 1000
+      , int32Val: -10000
+      , uint32Val: 10000
+      , floatVal: 1.25
+      , doubleVal: 1000.0005
+      , pointerVal: msTestPtr
+    })
+    assert.equal(100, ms.byteVal)
+    assert.equal(-100, ms.int8Val)
+    assert.equal(-1000, ms.int16Val)
+    assert.equal(1000, ms.uint16Val)
+    assert.equal(-10000, ms.int32Val)
+    assert.equal(10000, ms.uint32Val)
+    assert.equal(1.25, ms.floatVal)
+    assert.equal(1000.0005, ms.doubleVal)
+    assert.equal(ms.pointerVal.address(), msTestPtr.address())
+  })
+
+  it('should allow Struct nesting', function () {
+
+    var ChildStruct = new Struct([
+        ['int', 'a']
+      , ['int', 'b']
+    ])
+    var ParentStruct = new Struct([
+        [ChildStruct, 'childA']
+      , [ChildStruct, 'childB']
+    ])
+
+    var ps = new ParentStruct({
+        childA: { a: 100, b: 200 }
+      , childB: { a: 300, b: 400 }
+    })
+
+    assert.equal(100, ps.childA.a)
+    assert.equal(200, ps.childA.b)
+    assert.equal(300, ps.childB.a)
+    assert.equal(400, ps.childB.b)
+  })
+
+  describe('offsets and sizeofs', function () {
+
+    function test (structType, testNumber) {
+      describe('Struct test' + testNumber, function () {
+        it('should have a matching sizeof()', function () {
+          var expectedSize = bindings['test' + testNumber + ' sizeof']
+          assert.equal(expectedSize, structType.size, 'test' + testNumber +
+            ': sizeof(): expected ' + structType.size + ' to equal ' + expectedSize)
+        })
+        Object.keys(structType.fields).forEach(function (name) {
+          it('should have a matching offsetof() for "' + name + '"', function () {
+            var expectedOffset = bindings['test' + testNumber + ' offsetof ' + name]
+            var offset = structType.fields[name].offset
+            assert.equal(expectedOffset, offset, 'test' + testNumber + ': offsetof('
+                + name + '): expected ' + offset + ' to equal ' + expectedOffset)
+          })
+        })
+      })
+    }
+
+    var test1 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.int
+      , 'c': ref.types.double
+    })
+    test(test1, 1)
+
+    var test2 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.double
+      , 'c': ref.types.int
+    })
+    test(test2, 2)
+
+    var test3 = Struct({
+        'a': ref.types.double
+      , 'b': ref.types.int
+      , 'c': ref.types.int
+    })
+    test(test3, 3)
+
+    var test4 = Struct({
+        'a': ref.types.double
+      , 'b': ref.types.double
+      , 'c': ref.types.int
+    })
+    test(test4, 4)
+
+    var test5 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.double
+      , 'c': ref.types.double
+    })
+    test(test5, 5)
+
+    var test6 = Struct({
+        'a': ref.types.char
+      , 'b': ref.types.short
+      , 'c': ref.types.int
+    })
+    test(test6, 6)
+
+    var test7 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.short
+      , 'c': ref.types.char
+    })
+    test(test7, 7)
+
+    var test8 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.short
+      , 'c': ref.types.char
+      , 'd': ref.types.char
+    })
+    test(test8, 8)
+
+    var test9 = Struct({
+        'a': ref.types.int
+      , 'b': ref.types.short
+      , 'c': ref.types.char
+      , 'd': ref.types.char
+      , 'e': ref.types.char
+    })
+    test(test9, 9)
+
+    var test10 = Struct({
+        'a': test1
+      , 'b': ref.types.char
+    })
+    test(test10, 10)
+
+    var test11 = Struct()
+    test11.defineProperty('a', ref.types.size_t)
+    test11.defineProperty('b', ref.types.ushort)
+    test11.defineProperty('c', ref.types.ushort)
+    // this struct contains an Array of `test11 *` structs, so `test11 **`...
+    var test11_ptr_ptr = ref.refType(ref.refType(test11))
+    test11.defineProperty('d', test11_ptr_ptr)
+    test(test11, 11)
+
+  })
+
+})
---
+++ test/struct_tests.cc
@@ -0,0 +1,146 @@
+#include <stdlib.h>
+#include "v8.h"
+#include "node.h"
+
+using namespace v8;
+using namespace node;
+
+namespace {
+
+typedef struct _test1 {
+  int a;
+  int b;
+  double c;
+} test1;
+
+typedef struct _test2 {
+  int a;
+  double b;
+  int c;
+} test2;
+
+typedef struct _test3 {
+  double a;
+  int b;
+  int c;
+} test3;
+
+typedef struct _test4 {
+  double a;
+  double b;
+  int c;
+} test4;
+
+typedef struct _test5 {
+  int a;
+  double b;
+  double c;
+} test5;
+
+typedef struct _test6 {
+  char a;
+  short b;
+  int c;
+} test6;
+
+typedef struct _test7 {
+  int a;
+  short b;
+  char c;
+} test7;
+
+typedef struct _test8 {
+  int a;
+  short b;
+  char c;
+  char d;
+} test8;
+
+typedef struct _test9 {
+  int a;
+  short b;
+  char c;
+  char d;
+  char e;
+} test9;
+
+typedef struct _test10 {
+  test1 a;
+  char b;
+} test10;
+
+// this one simulates the `ffi_type` struct
+typedef struct _test11 {
+  size_t a;
+  unsigned short b;
+  unsigned short c;
+  struct _test11 **d;
+} test11;
+
+
+void Initialize(Handle<Object> target) {
+  HandleScope scope;
+
+  target->Set(String::NewSymbol("test1 sizeof"), Number::New(sizeof(test1)));
+  target->Set(String::NewSymbol("test1 offsetof a"), Number::New(offsetof(test1, a)));
+  target->Set(String::NewSymbol("test1 offsetof b"), Number::New(offsetof(test1, b)));
+  target->Set(String::NewSymbol("test1 offsetof c"), Number::New(offsetof(test1, c)));
+
+  target->Set(String::NewSymbol("test2 sizeof"), Number::New(sizeof(test2)));
+  target->Set(String::NewSymbol("test2 offsetof a"), Number::New(offsetof(test2, a)));
+  target->Set(String::NewSymbol("test2 offsetof b"), Number::New(offsetof(test2, b)));
+  target->Set(String::NewSymbol("test2 offsetof c"), Number::New(offsetof(test2, c)));
+
+  target->Set(String::NewSymbol("test3 sizeof"), Number::New(sizeof(test3)));
+  target->Set(String::NewSymbol("test3 offsetof a"), Number::New(offsetof(test3, a)));
+  target->Set(String::NewSymbol("test3 offsetof b"), Number::New(offsetof(test3, b)));
+  target->Set(String::NewSymbol("test3 offsetof c"), Number::New(offsetof(test3, c)));
+
+  target->Set(String::NewSymbol("test4 sizeof"), Number::New(sizeof(test4)));
+  target->Set(String::NewSymbol("test4 offsetof a"), Number::New(offsetof(test4, a)));
+  target->Set(String::NewSymbol("test4 offsetof b"), Number::New(offsetof(test4, b)));
+  target->Set(String::NewSymbol("test4 offsetof c"), Number::New(offsetof(test4, c)));
+
+  target->Set(String::NewSymbol("test5 sizeof"), Number::New(sizeof(test5)));
+  target->Set(String::NewSymbol("test5 offsetof a"), Number::New(offsetof(test5, a)));
+  target->Set(String::NewSymbol("test5 offsetof b"), Number::New(offsetof(test5, b)));
+  target->Set(String::NewSymbol("test5 offsetof c"), Number::New(offsetof(test5, c)));
+
+  target->Set(String::NewSymbol("test6 sizeof"), Number::New(sizeof(test6)));
+  target->Set(String::NewSymbol("test6 offsetof a"), Number::New(offsetof(test6, a)));
+  target->Set(String::NewSymbol("test6 offsetof b"), Number::New(offsetof(test6, b)));
+  target->Set(String::NewSymbol("test6 offsetof c"), Number::New(offsetof(test6, c)));
+
+  target->Set(String::NewSymbol("test7 sizeof"), Number::New(sizeof(test7)));
+  target->Set(String::NewSymbol("test7 offsetof a"), Number::New(offsetof(test7, a)));
+  target->Set(String::NewSymbol("test7 offsetof b"), Number::New(offsetof(test7, b)));
+  target->Set(String::NewSymbol("test7 offsetof c"), Number::New(offsetof(test7, c)));
+
+  target->Set(String::NewSymbol("test8 sizeof"), Number::New(sizeof(test8)));
+  target->Set(String::NewSymbol("test8 offsetof a"), Number::New(offsetof(test8, a)));
+  target->Set(String::NewSymbol("test8 offsetof b"), Number::New(offsetof(test8, b)));
+  target->Set(String::NewSymbol("test8 offsetof c"), Number::New(offsetof(test8, c)));
+  target->Set(String::NewSymbol("test8 offsetof d"), Number::New(offsetof(test8, d)));
+
+  target->Set(String::NewSymbol("test9 sizeof"), Number::New(sizeof(test9)));
+  target->Set(String::NewSymbol("test9 offsetof a"), Number::New(offsetof(test9, a)));
+  target->Set(String::NewSymbol("test9 offsetof b"), Number::New(offsetof(test9, b)));
+  target->Set(String::NewSymbol("test9 offsetof c"), Number::New(offsetof(test9, c)));
+  target->Set(String::NewSymbol("test9 offsetof d"), Number::New(offsetof(test9, d)));
+  target->Set(String::NewSymbol("test9 offsetof e"), Number::New(offsetof(test9, e)));
+
+  target->Set(String::NewSymbol("test10 sizeof"), Number::New(sizeof(test10)));
+  target->Set(String::NewSymbol("test10 offsetof a"), Number::New(offsetof(test10, a)));
+  target->Set(String::NewSymbol("test10 offsetof b"), Number::New(offsetof(test10, b)));
+
+  target->Set(String::NewSymbol("test11 sizeof"), Number::New(sizeof(test11)));
+  target->Set(String::NewSymbol("test11 offsetof a"), Number::New(offsetof(test11, a)));
+  target->Set(String::NewSymbol("test11 offsetof b"), Number::New(offsetof(test11, b)));
+  target->Set(String::NewSymbol("test11 offsetof c"), Number::New(offsetof(test11, c)));
+  target->Set(String::NewSymbol("test11 offsetof d"), Number::New(offsetof(test11, d)));
+
+}
+
+} // anonymous namespace
+
+NODE_MODULE(struct_tests, Initialize);
